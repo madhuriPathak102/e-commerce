8abfa8c724b749e553e03496c018fec582a0329f
pisc0036madhuripathak <piscmadhuripathak@outlook.com>
2024-07-09 00:32:01 +0530

    home and main

---
+++ src/app/home/main-carousel/main-carousel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-carousel',
+  templateUrl: './main-carousel.component.html',
+  styleUrls: ['./main-carousel.component.scss'],
+})
+export class MainCarouselComponent {}
