508068ce21dab8c1b5e0be005e8b5d434812420d
Madhuri Pathak <madhuripathak102@gmail.com>
2024-07-08 23:09:04 -0700

    PRODUCT DATA ADDED

--- src/app/home/main-carousel/main-carousel.component.ts
+++ src/app/home/main-carousel/main-carousel.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-carousel',
   templateUrl: './main-carousel.component.html',
   styleUrls: ['./main-carousel.component.scss'],
 })
-export class MainCarouselComponent {}
+export class MainCarouselComponent implements OnInit{
+  carouselData : any;
+  currentSlide=0;
+  interval:any;
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
